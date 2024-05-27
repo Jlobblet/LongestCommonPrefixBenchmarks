58fc33d5aacd9f3f63b4c2a9cc660b02b0bd5efb
John Blundell <jlobblet+github@jlobblet.co.uk>
2024-05-27 12:47:04 +0100

    Add WordLength parameter

--- LongestCommonPrefixBenchmarks/Benchmarks.cs
+++ LongestCommonPrefixBenchmarks/Benchmarks.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
 
 namespace LongestCommonPrefixBenchmarks;
 
@@ -6,6 +7,12 @@ namespace LongestCommonPrefixBenchmarks;
 [MaxRelativeError(0.02)]
 public class Benchmarks
 {
+    public enum WordLength
+    {
+        Short,
+        Long
+    }
+
     public enum WordSimilarityStrategy
     {
         NoneGuaranteed,
@@ -14,17 +21,52 @@ public class Benchmarks
         Identical
     }
 
-    private const int MinWordLength = 5;
-    private const int MaxWordLength = 20;
+    private const int ShortMinWordLength = 5;
+    private const int ShortMaxWordLength = 20;
+    private const int LongMinWordLength = 150;
+    private const int LongMaxWordLength = 200;
     private const string WordParts = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     private string[] _strings = null!;
 
     [ParamsAllValues] public WordSimilarityStrategy Strategy { get; set; }
 
+    [ParamsAllValues] public WordLength Length { get; set; }
+
     [Params(1000)] public int N { get; set; }
 
-    private static string GenerateRandomWord(int minLength = MinWordLength, int maxLength = MaxWordLength)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int MinWordLength(WordLength length) => length switch
+    {
+        WordLength.Short => ShortMinWordLength,
+        WordLength.Long => LongMinWordLength,
+        _ => throw new ArgumentOutOfRangeException(nameof(length))
+    };
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int MaxWordLength(WordLength length) => length switch
+    {
+        WordLength.Short => ShortMaxWordLength,
+        WordLength.Long => LongMaxWordLength,
+        _ => throw new ArgumentOutOfRangeException(nameof(length))
+    };
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int MinWordLength() => MinWordLength(Length);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int MaxWordLength() => MaxWordLength(Length);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string GenerateRandomWord(WordLength length) =>
+        length switch
+        {
+            WordLength.Short => GenerateRandomWord(ShortMinWordLength, ShortMaxWordLength),
+            WordLength.Long => GenerateRandomWord(LongMinWordLength, LongMaxWordLength),
+            _ => throw new ArgumentOutOfRangeException(nameof(length))
+        };
+
+    private static string GenerateRandomWord(int minLength, int maxLength)
     {
         var length = Random.Shared.Next(minLength, maxLength);
         var chars = Enumerable.Range(0, length).Select(_ => WordParts[Random.Shared.Next(WordParts.Length)]).ToArray();
@@ -34,27 +76,30 @@ public class Benchmarks
     [GlobalSetup]
     public void GlobalSetup()
     {
-        _strings = GenerateStringArray(N, Strategy);
+        _strings = GenerateStringArray(N, Strategy, Length);
     }
 
-    public static string[] GenerateStringArray(int n, WordSimilarityStrategy strategy)
+    public static string[] GenerateStringArray(int n, WordSimilarityStrategy strategy, WordLength length)
     {
         string[] res;
         switch (strategy)
         {
             case WordSimilarityStrategy.NoneGuaranteed:
-                res = Enumerable.Range(0, n).Select(_ => GenerateRandomWord()).ToArray();
+                res = Enumerable.Range(0, n).Select(_ => GenerateRandomWord(length)).ToArray();
                 break;
             case WordSimilarityStrategy.NearStart:
             case WordSimilarityStrategy.NearEnd:
                 var mult = strategy == WordSimilarityStrategy.NearStart ? 0.25 : 0.75;
-                var prefixLength = (int)(MaxWordLength * mult);
+                var prefixLength = (int)(MaxWordLength(length) * mult);
                 var prefix = GenerateRandomWord(prefixLength, prefixLength);
+                var minLength = Math.Max(MinWordLength(length) - prefixLength, 0);
+                var maxLength = Math.Max(Math.Max(MaxWordLength(length) - prefixLength, 0), minLength);
                 res = Enumerable.Repeat(prefix, n)
-                    .Select(p => p + GenerateRandomWord(Math.Max(MinWordLength - prefixLength, 0), MaxWordLength - prefixLength)).ToArray();
+                    .Select(p => p + GenerateRandomWord(minLength, maxLength))
+                    .ToArray();
                 break;
             case WordSimilarityStrategy.Identical:
-                var word = GenerateRandomWord();
+                var word = GenerateRandomWord(length);
                 res = Enumerable.Repeat(word, n).ToArray();
                 break;
             default:
--- LongestCommonPrefixTests/ValidStringArb.cs
+++ LongestCommonPrefixTests/ValidStringArb.cs
@@ -10,6 +10,7 @@ public static class ValidStringArb
         Gen.Elements(Benchmarks.WordSimilarityStrategy.NoneGuaranteed, Benchmarks.WordSimilarityStrategy.NearStart,
                 Benchmarks.WordSimilarityStrategy.NearEnd, Benchmarks.WordSimilarityStrategy.Identical)
             .Zip(Gen.Choose(10, 1000))
-            .Select(pair => new ValidStrings(Benchmarks.GenerateStringArray(pair.Item2, pair.Item1)))
+            .Select(pair =>
+                new ValidStrings(Benchmarks.GenerateStringArray(pair.Item2, pair.Item1, Benchmarks.WordLength.Short)))
             .ToArbitrary();
 }
